eb7c9dbd31988c7f9d8854ed9cc7962e78a42275
divinechukwuma <divinechukwuma982@gmail.com>
2024-03-27 11:43:57 +0100

    collected the variables

---
+++ game.js
@@ -0,0 +1,14 @@
+let buttonPaper = document.querySelector("#paper");
+let buttonRock = document.querySelector("#rock")
+let buttonScissors = document.querySelector("#scissors")
+
+let firstPlayer = document.querySelector("#secondPlayer")
+
+
+let computer = document.querySelector("#computer")
+
+
+
+// console.log( buttonPaper);
+// console.log(buttonRock);
+// console.log( buttonScissors);
